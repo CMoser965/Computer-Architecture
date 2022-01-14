b7f89199868a765ce526a085593e83857822515d
Cook <icook@okstate.edu>
2022-01-14 15:10:51 -0600

    end of L1-D1

--- Lab 1/regfile.sv
+++ Lab 1/regfile.sv
@@ -1,8 +1,8 @@
-module regfile(input  logic        clk, 
-               input  logic        we3, 
-               input  logic [4:0]  ra1, ra2, wa3, 
-               input  logic [31:0] wd3, 
-               output logic [31:0] rd1, rd2);
+module regfile(input  logic        clk, //clock 
+               input  logic        we3, //write enable signal
+               input  logic [4:0]  ra1, ra2, wa3, //source reg # read port x2, destination regwrite port 
+               input  logic [31:0] wd3, //data port for write
+               output logic [31:0] rd1, rd2); //data port for read
 
   logic [31:0]     rf[31:0];
 
@@ -10,6 +10,11 @@ module regfile(input  logic        clk,
   // read two ports combinationally
   // write third port on rising edge of clock
   // register 0 hardwired to 0
+  
+  //if we3 is enabled -> writes synchronously on rising edge of clk
+  
+  
+  //
 
 
 endmodule // regfile
