4dc69036dd8c456461705657dbeb75867adcc26f
ICook <isabellcook001@gmail.com>
2022-02-18 14:15:17 -0600

    AND Process completed

--- Lab 2/src/isa.h
+++ Lab 2/src/isa.h
@@ -18,10 +18,10 @@
 #define C_NXT ( (NEXT_STATE.CPSR>>29) & 0x00000001 )
 #define V_NXT ( (NEXT_STATE.CPSR>>28) & 0x00000001 )
 
-#define N_N 0x80000000
-#define Z_N 0x40000000
-#define C_N 0x20000000
-#define V_N 0x10000000
+#define N_N 0x80000000 //negative
+#define Z_N 0x40000000 //zero
+#define C_N 0x20000000 //carry
+#define V_N 0x10000000 //overflow
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,55 +38,58 @@
  */
 int AND (int Rd, int Rn, int Operand2, int I, int S, int CC){
   // Rd <- Rn & Src2(which is Operand2)
-
-  // Code below is ADD -> NOT FINISHED!!!!
-  //Rd <- Rn + Src2(which is Operand2)
   int cur = 0;
   if(I == 0){ //Register or Register-Shifted Register
-
-    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7; // shift amount (5 bit unsighed integer)
     int sh = (Operand2 & 0x00000060) >> 5;
+      /*
+        00 -> (0) -> logical left
+        01 -> (1) -> logical right
+        10 -> (2) -> arithmetic right
+        11 -> (3) -> rotate right
+
+      */
     int Rm = Operand2 & 0x0000000F;
     int Rs = (Operand2 & 0x00000F00) >> 8;
-    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int bit4 = (Operand2 & 0x00000010) >> 4;  
 
-    if (bit4 == 0) { //0: Register-Shifted Register
+    if (bit4 == 0) { //0: Register-Shifted Register (immediate shift value)
       switch (sh) {
-      case 0: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] << shamt5);
-        break;
-      case 1: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] >> shamt5);
-        break;
-      case 2: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] >> shamt5);
-        break;
-      case 3: 
-        cur = CURRENT_STATE.REGS[Rn] + ((CURRENT_STATE.REGS[Rm] >> shamt5) | (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
-        break;
+        case 0: //logical left shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] << shamt5);
+          break;
+        case 1: //logical right shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] >> shamt5);
+          break;
+        case 2: //arithmetic right shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] >> shamt5);
+          break;
+        case 3: //rotate right shift
+          cur = CURRENT_STATE.REGS[Rn] & ((CURRENT_STATE.REGS[Rm] >> shamt5) | (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+          break;
       }
-    } else { //1: Register 
+    } else { //1: Register
       switch (sh) {
-      case 0: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
-        break;
-      case 1: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
-        break;
-      case 2: 
-        cur = CURRENT_STATE.REGS[Rn] + (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
-        break;
-      case 3: 
-        cur = CURRENT_STATE.REGS[Rn] + ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) | (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
-	      break;
+        case 0: //logical left shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+          break;
+        case 1: //logical right shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+          break;
+        case 2: //arithmetic right shift
+          cur = CURRENT_STATE.REGS[Rn] & (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+          break;
+        case 3: //rotate right shift
+          cur = CURRENT_STATE.REGS[Rn] & ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) | (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+          break;
       }     
     }
   } else if (I == 1){  //Immediate
     int rotate = Operand2 >> 8;
     int Imm = Operand2 & 0x000000FF;
-    cur = CURRENT_STATE.REGS[Rn] + (Imm>>2*rotate|(Imm<<(32-2*rotate)));
-
+    cur = CURRENT_STATE.REGS[Rn] & (Imm>>2*rotate|(Imm<<(32-2*rotate)));
   }
+
   NEXT_STATE.REGS[Rd] = cur;
   if (S == 1) {
     if (cur < 0) {
